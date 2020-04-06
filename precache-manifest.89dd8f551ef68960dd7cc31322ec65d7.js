c0899f7191ed581b4ceaa3b539cc69a92fa1ecfd
WerHawer <werhawer@gmail.com>
2020-04-07 00:39:04 +0300

    Updates

---
+++ precache-manifest.89dd8f551ef68960dd7cc31322ec65d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcbf7a9e871ee6c52ffe2f432b4a543d",
+    "url": "/react_hw1/index.html"
+  },
+  {
+    "revision": "b8d8a3c11e95153b6971",
+    "url": "/react_hw1/static/js/2.69390098.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_hw1/static/js/2.69390098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa199ca0cec4f1e5a16e",
+    "url": "/react_hw1/static/js/main.bb1ad863.chunk.js"
+  },
+  {
+    "revision": "1535c7c480033bafe69b",
+    "url": "/react_hw1/static/js/runtime-main.0d03a759.js"
+  }
+]);
